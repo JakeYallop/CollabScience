e2a90f5ca82f0549b470d7c8094d01282f1d247f
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2023-10-07 19:18:50 +0100

    Bind to simple match parameters object

--- src/CollabScience.Api/Routes/MatchApi.cs
+++ src/CollabScience.Api/Routes/MatchApi.cs
@@ -3,13 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CollabScience.Api.Routes;
 
+public class MatchParametersData
+{
+    public string[]? AreasOfInterest { get; init; }
+    public string[]? Equipment { get; init; }
+    public string[]? Expertise { get; init; }
+    public int[]? AlreadyMatched { get; init; }
+}
+
 public static class MatchApi
 {
     public static RouteGroupBuilder MapMatchApiEndpoints(this RouteGroupBuilder group)
     {
-        group.MapPost("", (MatchParameters parameters, MatchingService matchingService) =>
+        group.MapPost("", (MatchParametersData parameters, MatchingService matchingService) =>
         {
-            return matchingService.ComputeMatchAsync(parameters);
+            return matchingService.ComputeMatchAsync(new(
+                parameters.AreasOfInterest ?? Array.Empty<string>(),
+                parameters.Equipment ?? Array.Empty<string>(),
+                parameters.Expertise ?? Array.Empty<string>(),
+                parameters.AlreadyMatched ?? Array.Empty<int>()
+            ));
         });
 
         return group;
