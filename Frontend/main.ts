15e75322a9654141e3b11959aa6092de194af774
Dolorious <doroteyastoyanova555@gmail.com>
2023-10-07 19:24:48 +0100

    select multiples

--- Frontend/main.ts
+++
@@ -1,9 +0,0 @@
-document.querySelector('.dropbtn')!.addEventListener('click', function () {
-    document.getElementById('expertiseDropdown')!.style.display = 'block';
-});
-
-document.addEventListener('click', function (e: MouseEvent) {
-    if (!document.querySelector('.dropdown')!.contains(e.target as Node)) {
-        document.getElementById('expertiseDropdown')!.style.display = 'none';
-    }
-});
