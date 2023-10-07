e136c0bc917abe6c6fc820b3cc5a7b978ecd07e0
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2023-10-07 13:23:26 +0100

    Move hello world endpoint

--- src/NovaScience.Api/Program.cs
+++ src/NovaScience.Api/Program.cs
@@ -32,7 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("api/", () => "Hello World!");
 
 app.MapFallbackToFile("index.html");
 
