5af9129b899e1d47ea1abf8158648ad6080f8d98
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2023-10-08 15:12:06 +0100

    Styling improvements

--- src/CollabScience.Api/Scripts/profileCheck.ts
+++ src/CollabScience.Api/Scripts/profileCheck.ts
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const profile = getProfileInformation();
     if (!profile) {
         const noProfileMessage = `
-        <div style="position: absolute; top: 0.5em; left: 35%;">
+        <div style="position: absolute; top: 0.5em; left: 35%; z-index: 99999">
             <h2>No profile found, <a href="/profile">go to the profile page to setup a profile</a></h2>
         </div>`;
         document.body.insertAdjacentHTML("afterbegin", noProfileMessage);
