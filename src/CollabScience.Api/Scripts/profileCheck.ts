cf67d652130251ee4e0bfdc13a232398136c962e
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2023-10-08 15:04:23 +0100

    Create profileCheck component

---
+++ src/CollabScience.Api/Scripts/profileCheck.ts
@@ -0,0 +1,12 @@
+import { getProfileInformation } from "./profile.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const profile = getProfileInformation();
+    if (!profile) {
+        const noProfileMessage = `
+        <div style="position: absolute; top: 0.5em; left: 35%;">
+            <h2>No profile found, <a href="/profile">go to the profile page to setup a profile</a></h2>
+        </div>`;
+        document.body.insertAdjacentHTML("afterbegin", noProfileMessage);
+    }
+});
